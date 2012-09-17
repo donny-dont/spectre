5cd88a61becfba0d46d2a335e987b12c28059698
John McCutchan <john@johnmccutchan.com>
2012-09-16 20:20:06 -0700

    WIP Material
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- lib/spectre_scene/material.dart
+++ lib/spectre_scene/material.dart
@@ -1,11 +1,41 @@
 
 class Material extends SceneChild {
+  int vertexShaderHandle;
+  int fragmentShaderHandle;
   int shaderProgramHandle;
   Map<String, int> uniforms;
   
-  Material(String name, Scene scene) : super(name, scene);
+  Material(String name, Scene scene) : super(name, scene) {
+    vertexShaderHandle = 0;
+    fragmentShaderHandle = 0;
+    shaderProgramHandle = 0;
+    uniforms = new Map<String, int>();
+  }
   
   void load(Map o) {
-    uniforms = new Map<String, int>();
+    int handle = scene.resourceManager.getResourceHandle(name);
+    ShaderProgramResource spr = scene.resourceManager.getResource(handle);
+    if (spr == null) {
+      spectreLog.Error('Could not load $name');
+      return;
+    }
+    if (vertexShaderHandle == 0) {
+      vertexShaderHandle = scene.device.createVertexShader('${name}.vs', {});
+    }
+    if (fragmentShaderHandle == 0) {
+      fragmentShaderHandle = scene.device.createFragmentShader('${name}.fs', {});
+    }
+    if (shaderProgramHandle == 0) {
+      shaderProgramHandle = scene.device.createShaderProgram('${name}.shader', {});  
+    }
+    
+    scene.device.getDeviceChild(vertexShaderHandle).source = spr.vertexShaderSource;
+    scene.device.getDeviceChild(vertexShaderHandle).compile();
+    scene.device.getDeviceChild(fragmentShaderHandle).source = spr.fragmentShaderSource;
+    scene.device.getDeviceChild(fragmentShaderHandle).compile();
+    scene.device.configureDeviceChild(shaderProgramHandle, {
+      'VertexProgram': vertexShaderHandle,
+      'FragmentProgram': fragmentShaderHandle,
+    });
   }
 }
