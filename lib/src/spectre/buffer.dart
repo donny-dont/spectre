3b44b3ef68156126f494e45cae5717d8e57f36bc
John McCutchan <john@johnmccutchan.com>
2012-12-09 17:01:41 -0800

    Add utility methods to SpectreBuffer.
    Signed-off-by: John McCutchan <john@johnmccutchan.com>

--- lib/src/spectre/buffer.dart
+++ lib/src/spectre/buffer.dart
@@ -22,7 +22,17 @@ part of spectre;
 
 */
 
+/** A [SpectreBuffer] represents a buffer of memory on the GPU.
+ * A [SpectreBuffer] can only be constructed by constructing an [IndexBuffer]
+ * or a [VertexBuffer].
+ */
 class SpectreBuffer extends DeviceChild {
+  /** Hint that buffer data is used once and then discarded. */
+  static const UsageStream = WebGLRenderingContext.STREAM_DRAW;
+  /** Hint that buffer data is used few times and then discarded. */
+  static const UsageDynamic = WebGLRenderingContext.DYNAMIC_DRAW;
+  /** Hint that buffer data is used many times and never discarded. */
+  static const UsageStatic = WebGLRenderingContext.STATIC_DRAW;
   WebGLBuffer _buffer;
   int _target;
   int _param_target;
@@ -61,17 +71,70 @@ class SpectreBuffer extends DeviceChild {
         }
       }
     }
-
-    /*
-    WebGLBuffer oldBind = device.gl.getParameter(_param_target);
-    device.gl.bindBuffer(_target, _buffer);
-    device.gl.bufferData(_target, _size, _usage);
-    device.gl.bindBuffer(_target, oldBind);
-    */
   }
 
   void _destroyDeviceState() {
     device.gl.deleteBuffer(_buffer);
     super._destroyDeviceState();
+  }
+
+  void _uploadData(dynamic data, int usage) {
+    device.gl.bufferData(_target, data, usage);
+  }
+
+  /** Resize buffer to fit [data]. Upload [data] with [usage] hint. */
+  void uploadData(dynamic data, int usage) {
+    var oldBind = device.gl.getParameter(_param_target);
+    device.gl.bindBuffer(_target, _buffer);
+    _uploadData(data, usage);
+    device.gl.bindBuffer(_target, oldBind);
+  }
+
+  void _uploadSubData(int offset, dynamic data) {
+    device.gl.bufferSubData(_target, offset, data);
+  }
+
+  /** Starting at [offset], upload [data] into buffer.
+   * The length of [data] + offset must not exceed the size of the buffer
+   */
+  void uploadSubData(int offset, dynamic data) {
+    var oldBind = device.gl.getParameter(_param_target);
+    device.gl.bindBuffer(_target, _buffer);
+    _uploadSubData(offset, data);
+    device.gl.bindBuffer(_target, oldBind);
+  }
+
+  void _allocate(int size, int usage) {
+    device.gl.bufferData(_target, size, usage);
+  }
+
+  /** Resize buffer to be [size] bytes with [usage] hint. */
+  void allocate(int size, int usgae) {
+    var oldBind = device.gl.getParameter(_param_target);
+    device.gl.bindBuffer(_target, _buffer);
+    _allocate(size, usage);
+    device.gl.bindBuffer(_target, oldBind);
+  }
+
+  /** Query the GPU for the size of the buffer. Can be expensive */
+  int get size {
+    var r;
+    var oldBind = device.gl.getParameter(_param_target);
+    device.gl.bindBuffer(_target, _buffer);
+    r = device.gl.getBufferParameter(_target,
+                                     WebGLRenderingContext.BUFFER_SIZE);
+    device.gl.bindBuffer(_target, oldBind);
+    return r;
+  }
+
+  /** Query the GPU for the usage hint of the buffer. Can be expensive */
+  int get usage {
+    var r;
+    var oldBind = device.gl.getParameter(_param_target);
+    device.gl.bindBuffer(_target, _buffer);
+    r = device.gl.getBufferParameter(_target,
+                                     WebGLRenderingContext.BUFFER_USAGE);
+    device.gl.bindBuffer(_target, oldBind);
+    return r;
   }
 }
