9d0a81fb8ba444ca3830c4ceaf47db89979253d1
Don Olmstead <don.j.olmstead@gmail.com>
2013-02-23 19:18:42 -0800

    Starting to flesh out the UI

--- example/simple/skeletal_animation_cpu/sample.dart
+++ example/simple/skeletal_animation_cpu/sample.dart
@@ -20,9 +20,83 @@
 
 library skeletal_animation_cpu;
 
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
 import 'dart:html';
+import 'dart:math' as Math;
+import 'dart:async';
+import 'package:property_map/property_map.dart';
+import 'package:vector_math/vector_math.dart';
+import 'package:game_loop/game_loop.dart';
+import 'package:asset_pack/asset_pack.dart';
+import 'package:spectre/spectre.dart';
+import 'package:spectre/spectre_asset_pack.dart';
+
+//---------------------------------------------------------------------
+// Application source
+//---------------------------------------------------------------------
 
 part 'ui.dart';
 
+//---------------------------------------------------------------------
+// Global variables
+//---------------------------------------------------------------------
+
+/// Instance of the [Application].
+//Application _instance;
+/// Instance of the [ApplicationControls].
+ApplicationControls _applicationControls;
+/// Instance of the [GameLoop] controlling the application flow.
+GameLoop _gameLoop;
+/// Identifier of the [CanvasElement] the application is rendering to.
+final String _canvasId = '#backBuffer';
+
+//---------------------------------------------------------------------
+// GameLoop hooks
+//---------------------------------------------------------------------
+
+/// Callback for when the application should be updated.
+void onFrame(GameLoop gameLoop) {
+  //_instance.onUpdate(gameLoop.dt);
+}
+
+/// Callback for when the application should render.
+void onRender(GameLoop gameLoop) {
+  //_instance.onRender();
+}
+
+/// Callback for when the canvas is resized.
+void onResize(GameLoop gameLoop) {
+
+}
+
+/// Callback for when the pointer lock changes.
+///
+/// Used to show/hide the options UI.
+void onPointerLockChange(GameLoop gameLoop) {
+  if (gameLoop.pointerLock.locked) {
+    _applicationControls.hide();
+  } else {
+    _applicationControls.show();
+  }
+}
+
+/// Entrypoint for the application.
 void main() {
+  // Create the application controls
+  _applicationControls = new ApplicationControls();
+  _applicationControls.show();
+
+  // Get the canvas
+  CanvasElement canvas = query(_canvasId);
+
+  // Hook up the game loop
+  // The loop isn't started until the start method is called.
+  _gameLoop = new GameLoop(canvas);
+  _gameLoop.onResize = onResize;
+  _gameLoop.onUpdate = onFrame;
+  _gameLoop.onRender = onRender;
+  _gameLoop.onPointerLockChange = onPointerLockChange;
 }
--- example/simple/skeletal_animation_cpu/ui.dart
+++ example/simple/skeletal_animation_cpu/ui.dart
@@ -1,2 +1,34 @@
 part of skeletal_animation_cpu;
 
+class ApplicationControls {
+
+  static String _controlContainerId = '#ui_wrap';
+  static String _hideClassName = 'hide';
+  static String _showClassName = 'show';
+
+  DivElement _controlContainer;
+
+  /// Creates an instance of the [ApplicationControls] class.
+  ApplicationControls() {
+    _controlContainer = query(_controlContainerId);
+  }
+
+  /// Show the controls.
+  ///
+  /// Uses a CSS animation to display the application controls.
+  void show() {
+    // Initially display is set as none
+    _controlContainer.style.display = 'block';
+
+    _controlContainer.classes.remove(_hideClassName);
+    _controlContainer.classes.add(_showClassName);
+  }
+
+  /// Hide the controls.
+  ///
+  /// Uses a CSS animation to hide the application controls.
+  void hide() {
+    _controlContainer.classes.remove(_showClassName);
+    _controlContainer.classes.add(_hideClassName);
+  }
+}
