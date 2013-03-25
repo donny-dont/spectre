5c9b4ba85759fcf1de3e86be12783da2ce4f6644
Brandon Jones <bajones@chromium.org>
2013-03-24 22:47:48 -0700

    Fixed position and index generation

--- example/simple/simple_geometry/simple_geometry_screen.dart
+++ example/simple/simple_geometry/simple_geometry_screen.dart
@@ -176,13 +176,13 @@ class SimpleGeometryScreen extends DemoScreen {
     // used to create all the spheres.
     //
     // Create a unit sphere
-    num radius = 1.0;
+    num radius = 0.5;
     vec3 center  = new vec3.raw(2.0, 0.0, 0.0);
 
-    InputLayoutElement positionElement = new InputLayoutElement(0, 1,  0, 12, GraphicsDevice.DeviceFormatFloat3);
-    InputLayoutElement normalElement   = new InputLayoutElement(0, 0, 12, 24, GraphicsDevice.DeviceFormatFloat3);
+    InputLayoutElement positionElement = new InputLayoutElement(0, 0,  0, 12, GraphicsDevice.DeviceFormatFloat3);
+    //InputLayoutElement normalElement   = new InputLayoutElement(0, 0, 12, 24, GraphicsDevice.DeviceFormatFloat3);
 
-    List<InputLayoutElement> elements = [positionElement, normalElement];
+    List<InputLayoutElement> elements = [positionElement]; //, normalElement];
 
     return SphereGenerator.createSphere('SphereGeometry', _graphicsDevice, elements, radius, center);
   }
--- lib/src/spectre_mesh/sphere_generator.dart
+++ lib/src/spectre_mesh/sphere_generator.dart
@@ -35,42 +35,33 @@ class SphereGenerator extends MeshGenerator {
    *
    * For the amount of storage space see [vertexBufferSize].
    */
-  int get vertexCount => (lonSegments * (latSegments - 1)) + 2;
+  int get vertexCount => ((lonSegments * (latSegments - 1)) + 2);
 
   /**
    * Retrieves the index buffer size necessary to hold the generated mesh.
    */
   int get indexCount => 6 * lonSegments * latSegments;
-
-  void generate(Float32Array vertexBuffer, Int16Array indexBuffer, List<InputLayoutElement> elements, [int vertexOffset = 0, int indexOffset = 0]) {
-    _vertexData = new VertexData(vertexBuffer, elements);
-  
-    Vector3Array positions = _vertexData.elements['vPosition'];
-    _generatePositions(positions, center, vertexOffset);
-    
-    _generateIndices(indexBuffer, vertexOffset, indexOffset);
-  }
   
   /**
    * Generates positional data.
    */
   void _generatePositions(Vector3Array positions, vec3 center, int vertexOffset) {
-    positions[vertexOffset++] = new vec3.raw(0, radius, 0) + center;
+    positions[vertexOffset++] = new vec3.raw(center.x, center.y + radius, center.z);
 
-    for (int y = 1; y < latSegments-1; ++y) {
-      for (int x = 0; x <= lonSegments; ++x) {
+    for (int y = 1; y < latSegments; ++y) {
+      for (int x = 0; x < lonSegments; ++x) {
         num u = x / lonSegments;
         num v = y / latSegments;
 
         positions[vertexOffset++] = new vec3.raw(
-            radius * cos(u * Math.PI * 2) * sin(v * Math.PI),
-            radius * cos(v * Math.PI),
-            radius * sin(u * Math.PI * 2) * sin(v * Math.PI)
-        ) + center;
+            radius * cos(u * Math.PI * 2.0) * sin(v * Math.PI) + center.x,
+            radius * cos(v * Math.PI) + center.y,
+            radius * sin(u * Math.PI * 2.0) * sin(v * Math.PI) + center.z
+        );
       }
     }
 
-    positions[vertexOffset++] = new vec3.raw(0, -radius, 0) + center;
+    positions[vertexOffset++] = new vec3.raw(center.x, center.y - radius, center.z);
   }
   
   /**
@@ -81,8 +72,8 @@ class SphereGenerator extends MeshGenerator {
     
     texCoords[vertexOffset++] = new vec2.raw(0.5, 0);
     
-    for (int y = 1; y < latSegments-1; ++y) {
-      for (int x = 0; x <= lonSegments; ++x) {
+    for (int y = 1; y < latSegments; ++y) {
+      for (int x = 0; x < lonSegments; ++x) {
         num u = x / lonSegments;
         num v = y / latSegments;
         texCoords[vertexOffset++] = new vec2.raw(u, v);
@@ -99,85 +90,64 @@ class SphereGenerator extends MeshGenerator {
     int x;
     
     // First ring
-    for(x = 1; x < lonSegments; ++x) {
-      indexBuffer[indexOffset++] = vertexOffset;
-      indexBuffer[indexOffset++] = vertexOffset + x;
-      indexBuffer[indexOffset++] = vertexOffset + x + 1;
+    for(x = 0; x < lonSegments-1; ++x) {
+      indexBuffer[indexOffset++] = 0;
+      indexBuffer[indexOffset++] = x + 2;
+      indexBuffer[indexOffset++] = x + 1;
     }
-    indexBuffer[indexOffset++] = vertexOffset;
-    indexBuffer[indexOffset++] = vertexOffset + x;
-    indexBuffer[indexOffset++] = vertexOffset + 1;
+    indexBuffer[indexOffset++] = 0;
+    indexBuffer[indexOffset++] = 1;
+    indexBuffer[indexOffset++] = x + 1;
     
     // Center rings
     int ring1Base, ring2Base;
     for (int y = 0; y < latSegments-2; ++y) {
-      ring1Base = vertexOffset + (lonSegments * y) + 1;
-      ring2Base = vertexOffset + (lonSegments * (y + 1)) + 1;
+      ring1Base = (lonSegments * y) + 1;
+      ring2Base = (lonSegments * (y + 1)) + 1;
       for (x = 0; x < lonSegments-1; ++x) {
         indexBuffer[indexOffset++] = ring1Base + x;
-        indexBuffer[indexOffset++] = ring2Base + x;
         indexBuffer[indexOffset++] = ring1Base + x + 1;
+        indexBuffer[indexOffset++] = ring2Base + x;
         
         indexBuffer[indexOffset++] = ring1Base + x + 1;
-        indexBuffer[indexOffset++] = ring2Base + x;
         indexBuffer[indexOffset++] = ring2Base + x + 1;
+        indexBuffer[indexOffset++] = ring2Base + x;
       }
       
       indexBuffer[indexOffset++] = ring1Base + x;
-      indexBuffer[indexOffset++] = ring2Base + x;
       indexBuffer[indexOffset++] = ring1Base;
+      indexBuffer[indexOffset++] = ring2Base + x;
       
       indexBuffer[indexOffset++] = ring1Base;
-      indexBuffer[indexOffset++] = ring2Base + x;
       indexBuffer[indexOffset++] = ring2Base;
+      indexBuffer[indexOffset++] = ring2Base + x;
     }
     
     // Last ring
-    int lastIndex = vertexOffset + (vertexCount - 1);
-    ring1Base = vertexOffset + lastIndex - (lonSegments + 1);
+    ring1Base = (lonSegments * (latSegments-2)) + 1;
+    ring2Base = (lonSegments * (latSegments-1)) + 1;
     for(x = 0; x < lonSegments-1; ++x) {
-      indexBuffer[indexOffset++] = lastIndex;
+      indexBuffer[indexOffset++] = ring2Base;
       indexBuffer[indexOffset++] = ring1Base + x;
       indexBuffer[indexOffset++] = ring1Base + x + 1;
     }
-    indexBuffer[indexOffset++] = lastIndex;
+    indexBuffer[indexOffset++] = ring2Base;
     indexBuffer[indexOffset++] = ring1Base + x;
     indexBuffer[indexOffset++] = ring1Base;
   }
 
-  /// Creates a single box with the given [extents] at the specified [center].
+  /// Creates a single sphere with the given [extents] at the specified [center].
   ///
   /// This is a helper method for creating a single sphere. If you are creating
   /// many sphere meshes prefer creating a [SphereGenerator] and using that to generate
   /// multiple meshes.
-  static Mesh createSphere(String name, GraphicsDevice graphicsDevice, List<InputLayoutElement> elements, num radius, [vec3 center, num latSegments = 6, num lonSegments = 8]) {
+  static Mesh createSphere(String name, GraphicsDevice graphicsDevice, List<InputLayoutElement> elements, num radius, vec3 center) {
+    // Setup the generator
     SphereGenerator generator = new SphereGenerator();
     generator.radius = radius;
-    generator.latSegments = latSegments;
-    generator.lonSegments = lonSegments;
-    
-    if(center != null) { 
-      generator.center = center;
-    }
-
-    // Create storage space for the vertices and indices
-    Float32Array vertices = new Float32Array(generator.vertexCount * 6);
-    Int16Array indices = new Int16Array(generator.indexCount);
-
-    // Generate the box
-    generator.generate(vertices, indices, elements);
-
-    // Upload the graphics data
-    VertexBuffer vertexBuffer = new VertexBuffer('${name}_VBO', graphicsDevice);
-    vertexBuffer.uploadData(vertices, SpectreBuffer.UsageStatic);
-
-    IndexBuffer indexBuffer = new IndexBuffer('${name}_IBO', graphicsDevice);
-    indexBuffer.uploadData(indices, SpectreBuffer.UsageStatic);
-
-    InputLayout inputLayout = new InputLayout('remove', graphicsDevice);
-    inputLayout.elements.addAll(elements);
 
     // Create the mesh
-    return new Mesh(name, graphicsDevice, vertexBuffer, inputLayout, indexBuffer);
+    Mesh mesh = MeshGenerator._createMesh(name, graphicsDevice, elements, generator, center);
+    return mesh;
   }
 }
